64d3550780f42869cc7d8ff8f5d09c596d21495c
Matthew Lerman <nerdpebble@gmail.com>
2021-05-27 16:24:47 -0400

    Web Scrape Price

--- webscraping/main.py
+++ webscraping/main.py
@@ -1,1 +1,37 @@
-print("Hello World")
+import requests 
+from bs4 import BeautifulSoup
+import time;
+import math;
+
+
+ticker = input("Input Ticker: ")
+
+URL = "https://finance.yahoo.com/quote/"
+
+ms = time.time() * 1000
+page = requests.get(URL + ticker.upper())
+
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+results = soup.find(id="quote-header-info")
+
+try:
+    text = results.find('div', class_="D(ib) Mend(20px)")
+except Exception:
+    print("Invalid Ticker!")
+    exit()
+
+price = ""
+text = str(text)
+for i in range(len(text) - 4):
+    if text[i] == '=' and text[i + 1] == '"' and text[i+2] == '5' and text[i+3] == "0":
+        x = 6
+        while text[i + x] != "<":
+            price += text[i + x]
+            x += 1
+        break
+
+ms2 = time.time() * 1000
+print("Price: $" + price + "\nTime Taken: " + str(math.ceil(ms2 - ms)) + " ms")
+
